11090e2b7d93e0ae5d8f6e969b13c656b5b85136
Unknown <zen.lednik7@gmail.com>
2018-06-28 13:44:49 +0200

    Moved restrictions into properties

--- lib/src/day_view/restrictions/restrictions.dart
+++
@@ -1,8 +0,0 @@
-library day_view_restrictions;
-
-import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
-
-import 'package:calendar_views/src/utils/all.dart';
-
-part 'day_view_restrictions.dart';
