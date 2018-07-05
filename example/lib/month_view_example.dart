b5dff40982026cc59494784be0b406cd254fcb7c
Unknown <zen.lednik7@gmail.com>
2018-07-05 14:02:28 +0200

    Added MonthView example

--- example/lib/month_view_example.dart
+++ example/lib/month_view_example.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import 'package:calendar_views/month_view.dart';
+import 'package:calendar_views/month_pager.dart';
+
+import 'weekday_drop_down_button.dart';
 
 class MonthViewExample extends StatefulWidget {
   @override
@@ -9,6 +12,23 @@ class MonthViewExample extends StatefulWidget {
 }
 
 class _MonthViewExampleState extends State<MonthViewExample> {
+  int _firstWeekday;
+
+  bool _showExtendedDaysBefore;
+  bool _showExtendedDaysAfter;
+
+  MonthPagerController _monthPagerController = new MonthPagerController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firstWeekday = DateTime.monday;
+
+    _showExtendedDaysBefore = true;
+    _showExtendedDaysAfter = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -18,9 +38,58 @@ class _MonthViewExampleState extends State<MonthViewExample> {
       body: new Column(
         children: <Widget>[
           new Expanded(
-            child: new MonthView(
-              month: new DateTime.now(),
-              dayOfMonthBuilder: _dayOfMonthBuilder,
+            child: new Container(
+              color: Colors.green.shade200,
+              child: new MonthView(
+                firstWeekday: _firstWeekday,
+                month: new DateTime.now(),
+                dayOfMonthBuilder: _dayOfMonthBuilder,
+                showExtendedDaysBefore: _showExtendedDaysBefore,
+                showExtendedDaysAfter: _showExtendedDaysAfter,
+              ),
+            ),
+          ),
+          new Expanded(
+            child: new SingleChildScrollView(
+              child: new Container(
+                padding: new EdgeInsets.all(16.0),
+                child: new Column(
+                  children: <Widget>[
+                    new ListTile(
+                      title: new Text("First WeekDay"),
+                      trailing: new WeekdayDropDownButton(
+                        value: _firstWeekday,
+                        onChanged: (value) {
+                          setState(() {
+                            _firstWeekday = value;
+                          });
+                        },
+                      ),
+                    ),
+                    new Divider(),
+                    new CheckboxListTile(
+                      title: new Text("Show Extended Days Before"),
+                      value: _showExtendedDaysBefore,
+                      onChanged: (value) {
+                        setState(() {
+                          _showExtendedDaysBefore = value;
+                        });
+                      },
+                    ),
+                    new Divider(),
+                    new CheckboxListTile(
+                      title: new Text("Show Extended Days After"),
+                      value: _showExtendedDaysAfter,
+                      onChanged: (value) {
+                        setState(() {
+                          _showExtendedDaysAfter = value;
+                        });
+                      },
+                    ),
+                    new Divider(),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
@@ -34,8 +103,9 @@ class _MonthViewExampleState extends State<MonthViewExample> {
       child: new Text(
         "${properties.date.day}",
         style: new TextStyle(
-            fontWeight:
-                properties.isExtended ? FontWeight.normal : FontWeight.bold),
+          fontWeight:
+              properties.isExtended ? FontWeight.normal : FontWeight.bold,
+        ),
       ),
     );
   }
