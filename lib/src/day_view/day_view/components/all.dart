f018dae126277d86188f39be33963f6a89583610
Unknown <zen.lednik7@gmail.com>
2018-08-30 14:23:31 +0200

    Added DaySeparationComponent

--- lib/src/day_view/day_view/components/all.dart
+++ lib/src/day_view/day_view/components/all.dart
@@ -1,3 +1,4 @@
+export 'day_separation_component/all.dart';
 export 'event_view_component/all.dart';
 export 'support_line_component/all.dart';
 export 'time_indication_component/all.dart';
@@ -5,6 +6,7 @@ export 'time_indication_component/all.dart';
 export 'day_view_component.dart';
 export 'item_position.dart';
 export 'item_size.dart';
+export 'positionable_item_builder.dart';
 export 'start_duration_positionable_item.dart';
 export 'time_positionable_item.dart';
 export 'time_positionable_item_builder.dart';
