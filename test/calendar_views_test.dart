798f761585df23695d0021746982176f64fde727
Unknown <zen.lednik7@gmail.com>
2018-09-20 14:01:38 +0200

    Renamed a group in month_page_view test

--- test/calendar_views_test.dart
+++ test/calendar_views_test.dart
@@ -1,7 +1,9 @@
 import '_internal_date_time/all.dart' as internal_date_time_test;
 import 'month_page_view/all.dart' as month_page_view;
+import 'month_view/all.dart' as month_view;
 
 void main() {
   internal_date_time_test.main();
   month_page_view.main();
+  month_view.main();
 }
--- test/month_page_view/all.dart
+++ test/month_page_view/all.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 import '_page_month.dart' as page_month;
 
 void main() {
-  group("MonthPageView test", () {
+  group("month_page_view test", () {
     page_month.main();
   });
 }
