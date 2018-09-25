47ed674320f28c5ebe49346ec47103e3b3e526b7
Unknown <zen.lednik7@gmail.com>
2018-09-25 18:09:40 +0200

    Better TimeIndicationComponent

--- lib/src/day_view/day_view_schedule/components/time_indication_component/time_indication_component.dart
+++ lib/src/day_view/day_view_schedule/components/time_indication_component/time_indication_component.dart
@@ -2,78 +2,165 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import 'package:calendar_views/day_view.dart';
+import 'package:calendar_views/src/_internal_date_time/all.dart';
 
+/// Signature for a function that builds a generated time indicator.
+typedef Positioned GeneratedTimeIndicatorItemBuilder(
+  BuildContext context,
+  ItemPosition itemPosition,
+  ItemSize itemSize,
+  int minuteOfDay,
+);
+
+/// [ScheduleComponent] for displaying time indicators in the [DayViewArea.timeIndicationArea] of [DayViewSchedule].
 @immutable
 class TimeIndicationComponent implements ScheduleComponent {
-  const TimeIndicationComponent({
+  /// Creates a [ScheduleComponent] that displays the given [timeIndicators].
+  ///
+  /// Time indicators are positioned as if they would start at
+  /// *[StartDurationItem.startMinuteOfDay] - ([StartDurationItem.duration] / 2)*.
+  /// (Center of the item will be at the position of [StartDurationItem.startMinuteOfDay])
+  TimeIndicationComponent({
     @required this.timeIndicators,
-    @required this.timeIndicatorItemBuilder,
   })  : assert(timeIndicators != null),
-        assert(timeIndicatorItemBuilder != null);
+        minuteOfDayOfFirstTimeIndicator = null,
+        timeIndicatorDuration = null,
+        generatedTimeIndicatorItemBuilder = null;
 
-  final List<ItemWithStartDuration> timeIndicators;
-  final ItemWithStartDurationBuilder timeIndicatorItemBuilder;
+  /// Creates a [ScheduleComponent] that generates and then displays time indicators.
+  ///
+  /// Time indicators are positioned as if they would start
+  /// *[timeIndicatorDuration] / 2* minutes earlier.
+  /// (Center of the item will be at the position of minute of day that the item represents)
+  TimeIndicationComponent.intervalGenerated({
+    this.minuteOfDayOfFirstTimeIndicator = 0,
+    this.timeIndicatorDuration = 60,
+    @required this.generatedTimeIndicatorItemBuilder,
+  })  : assert(minuteOfDayOfFirstTimeIndicator != null &&
+            isMinuteOfDayValid(minuteOfDayOfFirstTimeIndicator)),
+        assert(timeIndicatorDuration != null && timeIndicatorDuration > 0),
+        assert(generatedTimeIndicatorItemBuilder != null),
+        timeIndicators = null;
 
-  ItemPosition _getItemPosition({
-    @required SchedulingArea area,
-    @required ItemWithStartDuration item,
+  // provided time indicators --------------------------------------------------
+  /// List of time indicators to be displayed by this component.
+  final List<StartDurationItem> timeIndicators;
+
+  // generated time indicators -------------------------------------------------
+  /// Minute of day which the first generated time indicator will represent.
+  final int minuteOfDayOfFirstTimeIndicator;
+
+  /// Number of minutes between generated time indicators.
+  final int timeIndicatorDuration;
+
+  /// Function that builds a generated time indicator.
+  final GeneratedTimeIndicatorItemBuilder generatedTimeIndicatorItemBuilder;
+
+  bool get _shouldGenerateTimeIndicators => timeIndicators == null;
+
+  ItemPosition _makeItemPosition({
+    @required SchedulePositioner positioner,
+    @required int minuteOfDay,
+    @required int duration,
   }) {
-    assert(area != null);
-    assert(item != null);
+    int halfDuration = duration ~/ 2;
+    int adjustedMinuteOfDay = minuteOfDay - halfDuration;
 
     return new ItemPosition(
-      top: area.minuteOfDayFromTop(item.startMinuteOfDay),
-      left: area.left,
+      top: positioner.minuteOfDayFromTop(adjustedMinuteOfDay),
+      left: positioner.timeIndicationAreaLeft,
     );
   }
 
-  ItemSize _getItemSize({
-    @required SchedulingArea area,
-    @required ItemWithStartDuration properties,
+  ItemSize _makeItemSize({
+    @required SchedulePositioner positioner,
+    @required int duration,
   }) {
-    assert(area != null);
-    assert(properties != null);
-
     return new ItemSize(
-      width: area.size.width,
-      height: area.heightOfDuration(properties.duration),
+      width: positioner.timeIndicationAreaWidth,
+      height: positioner.heightOfDuration(duration),
     );
   }
 
   @override
-  List<Positioned> buildItems({
-    @required BuildContext context,
-    @required DayViewProperties properties,
-    @required SchedulePositioner positioner,
-  }) {
-    List<Positioned> builtItems = <Positioned>[];
-    SchedulingArea area = positioner.getNonNumberedArea(DayViewArea.timeIndicationArea);
-
-    for (ItemWithStartDuration item in timeIndicators) {
-      builtItems.add(
-        _buildItem(
-          context: context,
-          itemPosition: _getItemPosition(area: area, item: item),
-          itemSize: _getItemSize(area: area, properties: item),
-          item: item,
-        ),
+  List<Positioned> buildItems(
+    BuildContext context,
+    DayViewProperties properties,
+    SchedulePositioner positioner,
+  ) {
+    if (_shouldGenerateTimeIndicators) {
+      return _buildGeneratedTimeIndicators(
+        context: context,
+        positioner: positioner,
+      );
+    } else {
+      return _buildProvidedTimeIndicators(
+        context: context,
+        positioner: positioner,
       );
     }
-
-    return builtItems;
   }
 
-  Positioned _buildItem({
+  List<Positioned> _buildGeneratedTimeIndicators({
     @required BuildContext context,
-    @required ItemPosition itemPosition,
-    @required ItemSize itemSize,
-    @required ItemWithStartDuration item,
+    @required SchedulePositioner positioner,
   }) {
-    return timeIndicatorItemBuilder(
-      context: context,
-      position: itemPosition,
-      size: itemSize,
-      item: item,
-    );
+    List<Positioned> items = <Positioned>[];
+
+    for (int minuteOfDay = minuteOfDayOfFirstTimeIndicator;
+        minuteOfDay <= maximum_minute_of_day;
+        minuteOfDay += timeIndicatorDuration) {
+      ItemPosition itemPosition = _makeItemPosition(
+        positioner: positioner,
+        minuteOfDay: minuteOfDay,
+        duration: timeIndicatorDuration,
+      );
+
+      ItemSize itemSize = _makeItemSize(
+        positioner: positioner,
+        duration: timeIndicatorDuration,
+      );
+
+      Positioned item = generatedTimeIndicatorItemBuilder(
+        context,
+        itemPosition,
+        itemSize,
+        minuteOfDay,
+      );
+
+      items.add(item);
+    }
+
+    return items;
+  }
+
+  List<Positioned> _buildProvidedTimeIndicators({
+    @required BuildContext context,
+    @required SchedulePositioner positioner,
+  }) {
+    List<Positioned> items = <Positioned>[];
+
+    for (StartDurationItem timeIndicator in timeIndicators) {
+      ItemPosition itemPosition = _makeItemPosition(
+        positioner: positioner,
+        minuteOfDay: timeIndicator.startMinuteOfDay,
+        duration: timeIndicator.duration,
+      );
+
+      ItemSize itemSize = _makeItemSize(
+        positioner: positioner,
+        duration: timeIndicator.duration,
+      );
+
+      Positioned item = timeIndicator.builder(
+        context,
+        itemPosition,
+        itemSize,
+      );
+
+      items.add(item);
+    }
+
+    return items;
   }
 }
