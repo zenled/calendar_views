0fbebdb6f74a0b38a6fe74a63996c002e7c6848a
Unknown <zen.lednik7@gmail.com>
2018-09-26 19:40:03 +0200

    Removed unused example and rename

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,101 +2,70 @@ import 'package:flutter/material.dart';
 
 import 'days_page_view_example.dart';
 import 'day_view_example.dart';
-import 'month_example.dart';
 import 'month_page_view_example.dart';
 import 'month_view_example.dart';
 
 void main() => runApp(new MyApp());
 
+/// Screen for picking different examples of this library.
+///
+/// Examples:
+/// * [DayView example](day_view_example.dart)
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "calendar_views example",
       home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("calendar_views example"),
+        ),
         body: new Builder(builder: (BuildContext context) {
-          return new SingleChildScrollView(
-            child: new Container(
-              padding: new EdgeInsets.only(bottom: 16.0),
-              child: new Column(
-                children: <Widget>[
-                  new Container(
-                    margin: new EdgeInsets.only(bottom: 16.0),
-                    height: 250.0,
-                    color: Colors.green.shade200,
-                    child: new Center(
-                      child: new Text(
-                        "calendar_views \nExample",
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(fontSize: 30.0),
-                      ),
-                    ),
-                  ),
-                  new Divider(
-                    height: 0.0,
-                  ),
-                  new Divider(
-                    height: 0.0,
-                  ),
-                  new ListTile(
-                    title: new Text("Month"),
-                    subtitle: new Text("MonthPageView and MonthView"),
-                    onTap: () {
-                      _showWidgetInFullScreenDialog(
-                        context,
-                        new MonthExample(),
-                      );
-                    },
-                  ),
-                  new Container(
-                    height: 10.0,
-                    color: Colors.green.shade200,
-                  ),
-                  new ListTile(
-                    title: new Text("Day View"),
-                    subtitle: new Text("DayView Example"),
-                    onTap: () {
-                      _showWidgetInFullScreenDialog(
-                        context,
-                        new DayViewExample(),
-                      );
-                    },
-                  ),
-                  new Divider(height: 0.0),
-                  new ListTile(
-                    title: new Text("MonthView"),
-                    subtitle: new Text("MonthView Example"),
-                    onTap: () {
-                      _showWidgetInFullScreenDialog(
-                        context,
-                        new MonthViewExample(),
-                      );
-                    },
-                  ),
-                  new Divider(height: 0.0),
-                  new ListTile(
-                    title: new Text("DaysPageView"),
-                    subtitle: new Text("DaysPageView Example"),
-                    onTap: () {
-                      _showWidgetInFullScreenDialog(
-                          context, new DaysPageViewExample());
-                    },
-                  ),
-                  new Divider(height: 0.0),
-                  new ListTile(
-                    title: new Text("MonthPageView"),
-                    subtitle: new Text("MonthPageView Example"),
-                    onTap: () {
-                      _showWidgetInFullScreenDialog(
-                        context,
-                        new MonthPageViewExample(),
-                      );
-                    },
-                  ),
-                  new Divider(height: 0.0),
-                ],
+          return new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new Text("Day View"),
+                subtitle: new Text("DayView Example"),
+                onTap: () {
+                  _showWidgetInFullScreenDialog(
+                    context,
+                    new DayViewExample(),
+                  );
+                },
               ),
-            ),
+              new Divider(height: 0.0),
+              new ListTile(
+                title: new Text("MonthView"),
+                subtitle: new Text("MonthView Example"),
+                onTap: () {
+                  _showWidgetInFullScreenDialog(
+                    context,
+                    new MonthViewExample(),
+                  );
+                },
+              ),
+              new Divider(height: 0.0),
+              new ListTile(
+                title: new Text("DaysPageView"),
+                subtitle: new Text("DaysPageView Example"),
+                onTap: () {
+                  _showWidgetInFullScreenDialog(
+                      context, new DaysPageViewExample());
+                },
+              ),
+              new Divider(height: 0.0),
+              new ListTile(
+                title: new Text("MonthPageView"),
+                subtitle: new Text("MonthPageView Example"),
+                onTap: () {
+                  _showWidgetInFullScreenDialog(
+                    context,
+                    new MonthPageViewExample(),
+                  );
+                },
+              ),
+              new Divider(height: 0.0)
+            ],
           );
         }),
       ),
