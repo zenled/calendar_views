bdc7a3a705520f7c2b4c9013ba590609b40b8e61
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2020-07-11 18:57:14 -0400

    Example App Updated Gradle & Migration to AndroidX
    Changes:
    - Example App updated Gradle
    - Example App Migration to AndroidX

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/stephentan/Documents/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/stephentan/Documents/GitHub/calendar_views/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/stephentan/Documents/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.5.0"
+export "FLUTTER_BUILD_NUMBER=0.5.0"
